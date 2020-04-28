396cd46afa556ca25f4d6eefa3acad45a89c5fb1
FrostTusk <frosttusk@gmail.com>
2020-04-29 01:09:06 +0200

    finished echo server

--- Hooks/modules/basic/echo.js
+++ Hooks/modules/basic/echo.js
@@ -1,13 +1,13 @@
 const logging = require('../../middleware/logging.js');
 
 module.exports = function (app) {
-  app.post('/echo', (req, res) => {
-      logging.myLog({message: req.body, source: 'grappler'});
-      res.sendStatus(200);
+  app.get('/echo', (req, res) => {
+      logging.myLog({message: JSON.stringify(req.body), source: 'grappler'});
+      res.send(JSON.stringify(req.body));
   });
 
-  app.get('/echo', (req, res) => {
-      logging.myLog({message: req.body, source: 'grappler'});
-      res.sendStatus(200);
+  app.post('/echo', (req, res) => {
+      logging.myLog({message: JSON.stringify(req.body), source: 'grappler'});
+      res.send(JSON.stringify(req.body));
   });
 }
