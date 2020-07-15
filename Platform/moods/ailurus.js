a8fa767b32c883fe79cdba6eeb77ea88ac69a2be
FrostTusk <frosttusk@gmail.com>
2020-07-15 20:28:56 +0200

    got started on hdmi-cec set-up

--- Platform/moods/ailurus.js
+++ Platform/moods/ailurus.js
@@ -3,4 +3,5 @@ const logging = require("../middleware/logging.js");
 module.exports = function (app) {
   require("../tricks/staurois/arm-disarm.js")(app);
   require("../tricks/basic/echo.js")(app);
+  require("../tricks/hdmi-cec/taricha.js")(app);
 }
