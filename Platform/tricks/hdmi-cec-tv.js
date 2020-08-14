8236178726e7b3b430a82b2954570eabc6736270
FrostTusk <frosttusk@gmail.com>
2020-08-14 17:14:36 +0200

    Added state updating to hdmi-cec-tv trick

--- Platform/tricks/hdmi-cec-tv.js
+++ Platform/tricks/hdmi-cec-tv.js
@@ -1,19 +1,77 @@
 const { CEC, CECMonitor } = require("@senzil/cec-monitor");
 const logging = require('../middleware/logging.js');
+const http = require('http');
+
+function craftHTTPRequest(uriPath, body) {
+      let request = new http.ClientRequest({
+	      hostname: '192.168.222.164',
+	      port: 8123,
+	      path: uriPath,
+	      method: "POST",
+	      headers: {
+	        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkNDE5ZmFhODk3MzQ0NjViODMxZWRhMmRhYWEwYjc3NCIsImlhdCI6MTU5NzQxMTgzOSwiZXhwIjoxOTEyNzcxODM5fQ.r1LwULDi8gQ3b7jSNXITqrA7b1mJveOkJHPhFpzuQVU",
+	        "content-type": "application/json"
+	      }
+	  });
+	  request.end(body);
+}
+
 
 module.exports = function (app, tv_name='taricha') {
     //define listeners for cec on/off monitor.
     let monitor = new CECMonitor("G.I.N.G.E.R.", {});
-
-    monitor.on(CECMonitor.EVENTS.REPORT_POWER_STATUS, function (packet) {
-        console.log('packet', packet.data.str);
+    
+    monitor.on(CECMonitor.EVENTS.IMAGE_VIEW_ON, function(packet) {
+      console.log(packet);
+      let body = JSON.stringify({state: "on"});
+      craftHTTPRequest('/api/states/input_boolean.taricha', body);
     });
+
+    monitor.on(CECMonitor.EVENTS.REPORT_POWER_STATUS, function(packet) {
+      console.log(packet);
+      let body = JSON.stringify({state: "on"});
+      craftHTTPRequest('/api/states/input_boolean.taricha', body);
+    });
+
+    monitor.on(CECMonitor.EVENTS.STANDBY, function(packet) {
+       console.log(packet);
+       let body = JSON.stringify({state: "off"});
+       craftHTTPRequest('/api/states/input_boolean.taricha', body);
+    });
+
+    monitor.on(CECMonitor.EVENTS.ACTIVE_SOURCE, function(packet) {
+     console.log("Source DUDE");
+     console.log(packet);
+    });
+
+    /*monitor.on(CECMonitor.EVENTS.REPORT_POWER_STATUS, function (packet) {
+        console.log('packet', packet.data.str);
+        return;
+        let request = new http.ClientRequest({
+          hostname: '192.168.222.164',
+          port: 8123,
+          path: '/api/states/input_boolean.taricha',
+          method: "POST",
+          headers: {
+            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkNDE5ZmFhODk3MzQ0NjViODMxZWRhMmRhYWEwYjc3NCIsImlhdCI6MTU5NzQxMTgzOSwiZXhwIjoxOTEyNzcxODM5fQ.r1LwULDi8gQ3b7jSNXITqrA7b1mJveOkJHPhFpzuQVU",
+            "content-type": "application/json"
+	  }
+	});
+        
+	if (packet.data.str == "packet ON") {
+          let body = JSON.stringify({state: "on"});
+          request.end(body);
+	  console.log("WELL, WE GOT THIS FAR");
+	}
+    });*/
 
     app.post('/' + tv_name + '/on', (req, res) => {
         logging.myLog({source: 'command', tags: ['hdmi-cec-tv', tv_name], 
             message: "turned on"});
         monitor.WriteRawMessage('tx 40:04');
         res.sendStatus(200);
+        let body = JSON.stringify({state: "on"});
+        craftHTTPRequest('/api/states/input_boolean.taricha', body);
     });
 
     app.post('/' + tv_name + '/off', (req, res) => {
@@ -21,6 +79,8 @@ module.exports = function (app, tv_name='taricha') {
             message: "turned off"});
         monitor.WriteRawMessage('tx 40:36');
         res.sendStatus(200);
+        let body = JSON.stringify({state: "off"});
+        craftHTTPRequest('/api/states/input_boolean.taricha', body);
     });
 
     app.post('/' + tv_name + '/source', (req, res) => {
