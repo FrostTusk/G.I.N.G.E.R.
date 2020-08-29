e442d28514600ddbbf4d8b3615e668cc2ec60544
FrostTusk <frosttusk@gmail.com>
2020-08-29 23:53:31 +0200

    Started adding logging to HTTPInputTunnel

--- Platform/obstacles/tunnels/HTTPInputTunnel.js
+++ Platform/obstacles/tunnels/HTTPInputTunnel.js
@@ -1,26 +1,25 @@
 const InputTunnel = require('./InputTunnel.js');
 
 module.exports = class HTTPInputTunnel extends InputTunnel {
-    constructor(app, options, inputMood, authenticationHurdle, authMood) {
-      super(app, options, authenticationHurdle, authMood);
+    constructor(app, options, inputMood, authenticationHurdle, authMood, logTunnel) {
+      super(app, options, authenticationHurdle, authMood, logTunnel);
       this._procedure = function() {throw "procedure not specified"};
 
-      if (!inputMood) {
-        this._inputMood = (req, res) => {return req.body};
-      } else {
-        this._inputMood = inputMood;
-      }
+      this._inputMood = (inputMood) ? inputMood:
+        (req, res) => {return req.body};
+      this._authMood = (authMood) ? authMood: function() {};
+      this._logTunnel = logTunnel;
 
-      if (!authMood) {
-        this._authMood = function() {};
-      } else {
-        this._authMood = authMood;
-      }
+      if (logTunnel)
+        logTunnel.addTags(['HTTPInputTunnel']);
 
       let inputFunction;
       if (authenticationHurdle) {
         inputFunction = (req, res) => {
+          if (this._logTunnel)
+            this._logTunnel.emit('received request', ['no-auth']);
           this._authenticationHurdle.guard(this._authMood(req, res));
+
           this._procedure(this._inputMood(req, res));
           res.sendStatus(200);
         }
