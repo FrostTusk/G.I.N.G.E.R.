769e80c0032f2d97e3af6536aa267b986a94a62c
FrostTusk <frosttusk@gmail.com>
2020-08-30 22:51:15 +0200

    Added logging to CEC trick

--- Platform/tricks/HDMI-CEC-TVTrick.js
+++ Platform/tricks/HDMI-CEC-TVTrick.js
@@ -6,7 +6,7 @@ const { CEC, CECMonitor } = require("@senzil/cec-monitor");
  */
 module.exports = function (tv_name, monitor,
   turnOnInputTunnels, turnOffInputTunnels, switchSourceInputTunnels,
-  onListenerTunnels, offListenerTunnels, sourceListenerTunnels) {
+  onListenerTunnels, offListenerTunnels, sourceListenerTunnels, logTunnel) {
 
   if (false) {
       monitor.on(CECMonitor.EVENTS._OPCODE, function(packet) {
@@ -15,6 +15,8 @@ module.exports = function (tv_name, monitor,
   }
 
   monitor.on(CECMonitor.EVENTS.REPORT_POWER_STATUS, function(packet) {
+    if (logTunnel) logTunnel.emit('REPORT_POWER_STATUS: ' + packet.data.str, ['tricks']);
+
     if (packet.data.str === "ON") {
       for (let i in onListenerTunnels)
         onListenerTunnels[i].emit();
@@ -27,11 +29,15 @@ module.exports = function (tv_name, monitor,
   });
 
   monitor.on(CECMonitor.EVENTS.IMAGE_VIEW_ON, function(packet) {
+    if (logTunnel) logTunnel.emit('IMAGE_VIEW_ON: (TV is ON)', ['tricks']);
+
     for (let i in onListenerTunnels)
       onListenerTunnels[i].emit();
   });
 
   monitor.on(CECMonitor.EVENTS.STANDBY, function(packet) {
+    if (logTunnel) logTunnel.emit('STANDBY: (TV is OFF)', ['tricks']);
+
     for (let i in offListenerTunnels)
       offListenerTunnels[i].emit();
     for (let i in sourceListenerTunnels)
@@ -39,17 +45,22 @@ module.exports = function (tv_name, monitor,
   });
 
   monitor.on(CECMonitor.EVENTS.ACTIVE_SOURCE, function(packet) {
+    if (logTunnel) logTunnel.emit('ACTIVE_SOURCE: (TV is ON)', ['tricks']);
+
     for (let i in onListenerTunnels)
       onListenerTunnels[i].emit();
   });
 
   monitor.on(CECMonitor.EVENTS.REPORT_PHYSICAL_ADDRESS, function(packet) {
+    if (logTunnel) logTunnel.emit('REPORT_PHYSICAL_ADDRESS: source ' + packet.data.str[0], ['tricks']);
+
     for (let i in sourceListenerTunnels)
       sourceListenerTunnels[i].emit(packet.data.str[0]);
   });
 
   for (t in turnOnInputTunnels) {
     turnOnInputTunnels[t].on(() => {
+      if (logTunnel) logTunnel.emit('Turning TV on', ['tricks']);
       monitor.WaitForReady().then(() => monitor.WriteRawMessage('tx 40:04'));
       for (tunnel in onListenerTunnels)
         onListenerTunnels[tunnel].emit()
@@ -58,6 +69,7 @@ module.exports = function (tv_name, monitor,
 
   for (t in turnOffInputTunnels) {
     turnOffInputTunnels[t].on(() => {
+      if (logTunnel) logTunnel.emit('Turning TV off', ['tricks']);
       monitor.WaitForReady().then(() => monitor.WriteRawMessage('tx 40:36'));
       for (tunnel in offListenerTunnels)
         offListenerTunnels[tunnel].emit()
@@ -66,6 +78,7 @@ module.exports = function (tv_name, monitor,
 
   for (t in switchSourceInputTunnels) {
     switchSourceInputTunnels[t].on((source) => {
+      if (logTunnel) logTunnel.emit('Switch TV source to ' + source, ['tricks']);
       monitor.WaitForReady().then(() => monitor.WriteRawMessage('tx 4F:82:' + source + '0:00'));
       for (tunnel in onListenerTunnels)
         sourceListenerTunnels[tunnel].emit(source)
